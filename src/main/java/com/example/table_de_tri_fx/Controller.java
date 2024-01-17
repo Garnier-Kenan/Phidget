aa0866429eebc6237a927b1618b11621ac76da1a
K.N <125902437+garnier-kenan@users.noreply.github.com>
2024-01-17 09:29:16 +0100

    Add files via upload

---
+++ src/main/java/com/example/table_de_tri_fx/Controller.java
@@ -0,0 +1,22 @@
+package com.example.table_de_tri_fx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Button buttonconnexion;
+    public Label labelScann;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+
+    }
+
+  
+
+   
+}
